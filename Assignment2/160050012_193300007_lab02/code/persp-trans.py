4f570e7a460f8ce813ace26b8c86d0b258281242
Piyush Onkar <piyushonkar1998@gmail.com>
2021-02-07 22:35:54 +0530

    Need to document the code

--- Assignment2/160050012_193300007_lab02/code/persp-trans.py
+++ Assignment2/160050012_193300007_lab02/code/persp-trans.py
@@ -20,29 +20,51 @@ lowerLeft = [704,1028]
 lowerRight = [992,826]
 upperRight = [481,186]
 middle = [453,625]
-originalC = np.array([upperLeft,lowerLeft,lowerRight,upperRight],dtype='float32')
+originalC = np.array([upperLeft,lowerLeft,lowerRight,upperRight],dtype='uint8')
+
+
+cv2.imshow("Progression",img)
+cv2.waitKey(1000)
+
+upperLeft = [220,241] 
+lowerLeft = [704,1028]
+lowerRight = [992,826]
+upperRight = [481,186]
+originalC = np.array([upperLeft,lowerLeft,lowerRight,upperRight])
+stencil = np.zeros(img.shape).astype(img.dtype)
+color = [255, 255, 255]
+cv2.fillPoly(stencil, [originalC], color)
+result = cv2.bitwise_and(img, stencil)
+cv2.imshow("Progression", result)
+cv2.waitKey(1000)
+img = result
+
+originalC = originalC.astype('float32')
 newUpperLeft = [0,384]
 newUpperRight = [0,0]
 newLowerLeft =  [511,384]
 newLowerRight = [511,0]
-newMiddle = [256,384]
 newC = np.array([newUpperLeft,newLowerLeft,newLowerRight,newUpperRight],dtype='float32')
 updateArray = (newC-originalC)/8
 count = 8
 oldC = originalC
-cv2.imshow("Progression",img)
+
 while count > 0:
     newIntermediateC = oldC + updateArray
     matrix = cv2.getPerspectiveTransform(oldC,newIntermediateC)
     maxWidth = np.max(newIntermediateC,axis=0)[0]
     maxHeight = np.max(newIntermediateC,axis=0)[1]
-    result = cv2.warpPerspective(img,matrix,(maxWidth,maxHeight))
+    if count == 1:
+        result = cv2.warpPerspective(img,matrix,(512,385))
+    else:
+        result = cv2.warpPerspective(img,matrix,(maxWidth,maxHeight))
     cv2.imshow("Progression",result)
-    img = result    
+    img = result   
     count = count - 1
     oldC = newIntermediateC
     if cv2.waitKey(1000) & 0xFF == ord('q'):
         break
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # #### Transformation Matrix for shear transformation
